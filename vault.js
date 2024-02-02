fa28867d7a2f65145942614604022502f3cf9510
TPayne17 <156387213+TPayne17@users.noreply.github.com>
2024-02-01 23:08:33 -0600

    Add files via upload

---
+++ vault.js
@@ -0,0 +1,11 @@
+// Create a message string "Chosen to open an important vault, here is the secret combination"
+// Create 3 mathematical calclations to generate the 3 codes in the combo
+// The combination of the lock is 10 - 40 - 39. 
+
+alert("You have received this message because you have been chosen to open an important vault.  Here is the secret combination");
+
+
+
+
+
+
